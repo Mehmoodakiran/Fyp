9ebf9747af0b1054f5181f61cc3d3ebd0e633ecb
Mehmooda Kiran <mehmoodakiran9@gmail.com>
2023-12-19 14:56:20 +0500

    aboutscss

--- backend/models/reviewmodels.js
+++ backend/models/reviewmodels.js
@@ -1,31 +1,52 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-// Define a schema for the 'User' model
 const commentSchema = new mongoose.Schema({
-
-  // Field for the user's email
   userName: {
     type: String,
     required: true,
-    
   },
-
   text: {
     type: String,
     required: true,
   },
   rating: {
-    type: Number,
-    min: 0,
-    max: 5,
+    type: Number, // Change the type to Number to match the Rating model
+    ref: 'Rating',
   },
-
-
-
 });
 
-// Create the 'User' model based on the schema
+const Comment = mongoose.model('Comment', commentSchema);
 
-export default mongoose.model("Comment", commentSchema);
+export default Comment;
+
+// import mongoose from 'mongoose'
+
+// // Define a schema for the 'User' model
+// const commentSchema = new mongoose.Schema({
+
+//   // Field for the user's email
+//   userName: {
+//     type: String,
+//     required: true,
+    
+//   },
+
+//   text: {
+//     type: String,
+//     required: true,
+//   },
+//   rating: {
+//     type: Number,
+//     min: 0,
+//     max: 5,
+//   },
+
+
+
+// });
+
+// // Create the 'User' model based on the schema
+
+// export default mongoose.model("Comment", commentSchema);
 
 
